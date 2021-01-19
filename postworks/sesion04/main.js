422824930df70c7a48aed349ee6dcd813095f742
Nathaly N Dimas C <nathaly.ndc@hotmail.com>
2021-01-18 22:42:52 -0600

    ejercicio2Mejorado

--- postworks/sesion04/main.js
+++ postworks/sesion04/main.js
@@ -2,23 +2,19 @@
 
 //Ejercicio 2
 function chunk(array, size){
+    var arreglo = [...array];//copia de arreglo y no locación de memoria
     var nvo=[];
-    var nvo2=[];
-    for(var i=0;i<array.length;i++){
-        for(var j=0;j<size;j++){
-            if(i<array.length) nvo2.push(array[i]);
-             i++;
-        }
-        if(j==size && i<array.length) i--;
-        nvo.push(nvo2);
-        nvo2=[];
+    while(arreglo.length>0){
+        nvo.push(arreglo.splice(0,size)); //si ya no hay elementos corta hasta donde exista alguno
     }
     return nvo;
 }
+
+
 var data = [1, 2, 3, 4, 5, 6, 7, 8]
 
 console.log('Test 1:', chunk(data, 1)) // [[1], [2], [3], [4], [5], [6], [7], [8]]
 console.log('Test 2:', chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
 console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
 
-//--------------------------------------------------------
+//---------------------------------------------------------
