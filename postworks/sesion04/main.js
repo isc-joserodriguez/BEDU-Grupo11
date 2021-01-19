a419253c0be1faa3d8c59cc2a8c80a2a1342c75d
Nathaly N Dimas C <nathaly.ndc@hotmail.com>
2021-01-18 23:50:42 -0600

    Ejercicio3

--- postworks/sesion04/main.js
+++ postworks/sesion04/main.js
@@ -50,5 +50,28 @@ var data = [1, 2, 3, 4, 5, 6, 7, 8]
 console.log('Test 1:', chunk(data, 1)) // [[1], [2], [3], [4], [5], [6], [7], [8]]
 console.log('Test 2:', chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
 console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
-
+console.log("--------------------------------------------");
 //---------------------------------------------------------
+//Ejercicio 3
+function frequency(string) {
+    // Code goes here
+    var cadena = string.split('').sort(); //la cadena se pasa a arreglo de caracteres donde '' significa 1 a 1 por el vacío y ordenar
+    var result = {};
+    for(var i=0; i<cadena.length;i++){
+        if(result[cadena[i]] == undefined){//si la propiedad no existe la agrega con valor de 1
+            result[cadena[i]] = 1;
+          
+        }
+        else{
+           result[cadena[i]]++; //si ya existe le suma 1 por cada coincidencia
+        }
+    }
+    return result;
+   }
+   
+   console.log('Test 1:', frequency('cccbbbaaa'))
+   // {a: 3, b: 3, c: 3}
+   console.log('Test 2:', frequency('www.bedu.org'))
+   // {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
+   console.log('Test 3:', frequency('john.doe@domain.com'))
+   // {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
