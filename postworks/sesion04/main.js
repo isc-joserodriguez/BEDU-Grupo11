2fb1a440d40d163f6625fc7b9149418f09f939fd
Nathaly N Dimas C <nathaly.ndc@hotmail.com>
2021-01-18 23:19:39 -0600

    ejercicio1

--- postworks/sesion04/main.js
+++ postworks/sesion04/main.js
@@ -1,4 +1,38 @@
 //https://github.com/beduExpert/B1-Programacion-con-Javascript-2020-Santander/tree/main/Sesion-04/Postwork
+//Ejercicio 1
+function deepEqual(a, b) {
+    // Fancy
+    if(typeof(a)==="object" && typeof(b)==="object"){
+        var keysA = Object.keys(a).sort();
+        var keysB = Object.keys(b).sort();
+        
+        if(JSON.stringify(keysA).includes(JSON.stringify(keysB))) {
+           return  keysA.map(element => {
+               return deepEqual(a[element],b[element])
+            }).reduce((element,next) => {
+                return(element===next);
+            });
+        }
+        else{
+            return false;
+        }
+    }
+    else {
+        return a===b;
+    }
+  }
+  
+  var john = {
+    firstName: 'John',
+    lastName: 'Doe'
+  }
+  
+  console.log('Test 1:', deepEqual(1, 1)) // true
+  console.log('Test 2:', deepEqual(1, '1')) // false
+  console.log('Test 3:', deepEqual(john, john)) // true
+  console.log('Test 4:', deepEqual(john, { firstName: 'John', lastName: 'Doe' })) // true
+  console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
+  console.log("--------------------------------------------");
 
 //Ejercicio 2
 function chunk(array, size){
