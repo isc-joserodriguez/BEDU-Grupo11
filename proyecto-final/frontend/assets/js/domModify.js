c636e0c4315807df2c4ef993d143ab6f6d762fa5
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-05 16:57:56 -0700

    [TA002] - Added Functions to modify DOM

---
+++ proyecto-final/frontend/assets/js/domModify.js
@@ -0,0 +1,31 @@
+const createElement = (element, father) => {
+    let tempElement = document.createElement(element.elementType);
+    if (element.classes) tempElement.classList = element.classes.join(" ");
+    if (element.id) tempElement.id = element.id;
+    if (element.attributes) {
+        for (attribute in element.attributes) {
+            tempElement[attribute] = element.attributes[attribute];
+        }
+    }
+    if (element.elementEvents) {
+        element.elementEvents.forEach(elementEvent => {
+            tempElement.addEventListener(elementEvent.elementListener, elementEvent.elementFunction)
+        });
+    }
+    if (element.childs) addChilds(element.childs, tempElement);
+    father.appendChild(tempElement);
+}
+
+const addChilds = (arrayChilds, father) => {
+    arrayChilds.forEach(element => {
+        createElement(element, father);
+    });
+}
+
+const addClasses = (element, classes) => {
+    element.classList = [... new Set([...element.classList].concat(classes))].join(' ');
+}
+
+const removeClass = (element, removedClass) => {
+    element.classList = [...element.classList].filter(elementClass=>elementClass!==removedClass).join(' ');
+}
